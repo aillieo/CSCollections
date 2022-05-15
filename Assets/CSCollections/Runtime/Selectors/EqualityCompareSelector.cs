80478defa263557800a06a3715f9d7f319a557ca
aillieo <aillieo@163.com>
2022-05-15 21:13:37 +0800

    update

---
+++ Assets/CSCollections/Runtime/Selectors/EqualityCompareSelector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AillieoUtils.Collections
+{
+    public class EqualityCompareSelector<TSource, TTarget> : IEqualityComparer<TSource>
+    {
+        private readonly Func<TSource, TTarget> selector;
+
+        public EqualityCompareSelector(Func<TSource, TTarget> selector)
+        {
+            this.selector = selector;
+        }
+
+        public bool Equals(TSource x, TSource y)
+        {
+            return selector(x).Equals( selector(y));
+        }
+
+        public int GetHashCode(TSource obj)
+        {
+            return selector(obj).GetHashCode();
+        }
+    }
+}
