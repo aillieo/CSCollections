75c16724cf7fbefd92fa290b41c28b442d062a4b
aillieo <aillieo@163.com>
2022-05-15 21:13:37 +0800

    update

---
+++ CSCollections/Runtime/MultiDictionary.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AillieoUtils.Collections
+{
+    public class MultiDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, ICollection<TValue>>>, IDictionary<TKey, ICollection<TValue>>
+    {
+        public IEnumerator<KeyValuePair<TKey, ICollection<TValue>>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(KeyValuePair<TKey, ICollection<TValue>> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(KeyValuePair<TKey, ICollection<TValue>> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<TKey, ICollection<TValue>>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<TKey, ICollection<TValue>> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count { get; }
+        public bool IsReadOnly { get; }
+        public void Add(TKey key, ICollection<TValue> value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetValue(TKey key, out ICollection<TValue> value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<TValue> this[TKey key]
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public ICollection<TKey> Keys { get; }
+        public ICollection<ICollection<TValue>> Values { get; }
+    }
+}
---
+++ CSCollections/Runtime/Selectors/CompareSelector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AillieoUtils.Collections
+{
+    public class CompareSelector<TSource, TTarget> : IComparer<TSource where TTarget : IComparable<TTarget>
+    {
+        private readonly Func<TSource, TTarget> selector;
+
+        public CompareSelector(Func<TSource, TTarget> selector)
+        {
+            this.selector = selector;
+        }
+
+        public int Compare(TSource x, TSource y)
+        {
+            TTarget tx = selector(x);
+            TTarget ty = selector(y);
+            if (tx != null)
+            {
+                return tx.CompareTo(ty);
+            }
+            else if(ty != null)
+            {
+                return -ty.CompareTo(tx);
+            }
+
+            return 0;
+        }
+    }
+}
